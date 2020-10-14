c3cad7c0ccc08b8dacf85704f8b1dac97b923b9e
konradk89 <konrad.kurzawski@gmail.com>
2020-10-14 23:43:45 +0200

    feature with tests

--- src/main/java/com/kurzawsk/betting/common/Library.java
+++ src/main/java/com/kurzawsk/betting/common/Library.java
@@ -4,7 +4,7 @@
 package com.kurzawsk.betting.common;
 
 public class Library {
-    public boolean someLibraryMethod2() {
-        return false;
+    public static boolean someLibraryMethod2() {
+        return true;
     }
 }
